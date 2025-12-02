5ae8e6c2f6fe1e116cdd2d2e2c656d383ddaf43b
stanstanstanlee <stanstanstanlee@gmail.com>
2025-12-02 10:40:54 +0900

    read all

--- src/main/java/com/example/instagram/service/PostService.java
+++ src/main/java/com/example/instagram/service/PostService.java
@@ -4,9 +4,12 @@ import com.example.instagram.dto.request.PostCreateRequest;
 import com.example.instagram.dto.response.PostResponse;
 import com.example.instagram.entity.Post;
 
+import java.util.List;
+
 public interface PostService {
     PostResponse create(PostCreateRequest postCreateRequest, Long userId);
     Post findById(Long postId);
     PostResponse getPost(Long postId);
+    List<PostResponse> getAllPosts();
 
 }
