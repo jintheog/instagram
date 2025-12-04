898764c91de4693d1d36de782943a287604e5568
stanstanstanlee <stanstanstanlee@gmail.com>
2025-12-04 10:53:47 +0900

    Home 화면의 좋아요 count 댓글 count 출력

--- src/main/java/com/example/instagram/repository/CommentRepository.java
+++ src/main/java/com/example/instagram/repository/CommentRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPostIdOrderByCreatedAtDesc(Long postId);
+    long countByPostId(Long postId);
+
 }
