c178cf8e6e56a07a412f53dfa7a2ea623d3c5b4a
stanstanstanlee <stanstanstanlee@gmail.com>
2025-12-05 10:48:25 +0900

    유저 검색 기능

--- src/main/java/com/example/instagram/repository/UserRepository.java
+++ src/main/java/com/example/instagram/repository/UserRepository.java
@@ -1,12 +1,21 @@
 package com.example.instagram.repository;
 
+import com.example.instagram.dto.response.UserResponse;
 import com.example.instagram.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:keyword% OR u.name LIKE %:keyword%")
+    List<UserResponse> searchByKeyword(@Param("keyword") String keyword);
+
+
 }
--- src/main/java/com/example/instagram/service/UserService.java
+++ src/main/java/com/example/instagram/service/UserService.java
@@ -7,6 +7,8 @@ import com.example.instagram.dto.response.UserResponse;
 import com.example.instagram.entity.User;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface UserService {
 
     User register(SignUpRequest signUpRequest);
@@ -23,5 +25,6 @@ public interface UserService {
 
     void updateProfile(Long userId, ProfileUpdateRequest profileUpdateRequest, MultipartFile profileImg);
 
-
+    // 사용자 검색
+    List<UserResponse> searchUsers(String keyword);
 }
